610c02231c9768a8fcf1f2d3a7162ad3f163994b
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-23 12:18:20 -0300

    Basic creation of the Product and ProductApi classes

--- app/src/main/java/com/digital/applicationworkapi/MainActivity.kt
+++ app/src/main/java/com/digital/applicationworkapi/MainActivity.kt
@@ -2,10 +2,15 @@ package com.digital.applicationworkapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recycleProducts: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycleProducts = findViewById(R.id.recycler_products)
     }
 }
